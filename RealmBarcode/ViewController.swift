cc01605b5b1488e0e0a9e29551db19245bc8b9e1
David Spector <spector@zeitgeist.com>
2018-03-23 22:14:18 -0700

    Initial Commit

---
+++ RealmBarcode/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RealmBarcode
+//
+//  Created by David HM Spector on 3/23/18.
+//  Copyright © 2018 Realm. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
